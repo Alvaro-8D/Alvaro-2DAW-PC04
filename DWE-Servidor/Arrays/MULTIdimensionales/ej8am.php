5e89da313cfeb65304758671e3e79a578cbb35df
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-10-22 12:07:28 +0200

    Empezar Producto ej8 A.Multi de nuevo

--- DWE-Servidor/Arrays/MULTIdimensionales/ej8am.php
+++ DWE-Servidor/Arrays/MULTIdimensionales/ej8am.php
@@ -5,8 +5,8 @@
 <?php
     include '../../Otros/funciones.php'; // inlucye verTabla()verTabla($tabla);
 
-    $f = 3; // filas del array
-    $c = 1; // columnas del array
+    $f = 2; // filas del array
+    $c = 2; // columnas del array
 
     for ($i=0; $i < $f; $i++) {  // Matriz 1
         for ($j=0; $j < $c; $j++) {  
@@ -24,7 +24,7 @@
     verTabla($tabla);
     echo "<br><h2>Matriz 2</h2>";
     verTabla($tabla2);
-/****************************************** 
+
     foreach ($tabla as $i => $seccion) {
         foreach ($seccion as $j => $value) {
              $tabla3[$i][$j] += $value;
@@ -35,50 +35,8 @@
     echo "<br><h2>Suma</h2>"; // SUMA DE MATRICES
     verTabla($tabla3); 
 /* -------------------------------------------------------------------- */
-    $lista1; $lista2; // [a][b][c]
-    $lista3 = 0; // prducto de $lista1 y $lista2
-
-    for($i = 0; $i<count($tabla); $i++) {
-        $lista1[$i] = $tabla[$i][0];
-        echo "<br>\$i = ",$tabla[$i][0];
-    }
-    echo "<br>-----------------------------------<br>";
-    for($i = 0; $i<count($tabla2[0]); $i++) {
-        $lista2[$i] = $tabla2[0][$i];
-        echo "<br>\$i = ",$tabla2[0][$i];
-    }
-/* 00000000000000000000000000000000000000000000000000000000 */
-    for($i = 0; $i<count($tabla2); $i++) {
-        $lista3 +=  $lista1[$i] * $lista2[$i];
-    }
-
-    echo "<br>---->",$lista3;"<br>";
-/* 1111111111111111111111111111111111111111111111111111111 */
-    for($i = 0; $i<count($tabla2[0]); $i++) {
-        $lista2[$i] = $tabla2[0][$i];
-        echo "<br>\$i = ",$tabla2[0][$i];
-    }
-
-    for($i = 0; $i<count($tabla2); $i++) {
-        $lista3 +=  $lista1[$i] * $lista2[$i];
-    }
-
-    echo "<br>---->",$lista3;"<br>";
-/* 222222222222222222222222222222222222222222222222222222222 */
-    for($i = 0; $i<count($tabla2[0]); $i++) {
-        $lista2[$i] = $tabla2[0][$i];
-        echo "<br>\$i = ",$tabla2[0][$i];
-    }
-
-    for($i = 0; $i<count($tabla2); $i++) {
-        $lista3 +=  $lista1[$i] * $lista2[$i];
-    }
-
-    echo "<br>---->",$lista3;"<br>";
     
-
-
-    echo "<br><h2>Producto</h2>";// FALTA HAACER EL PRODUCTO
+    echo "<br><h2>Producto</h2>";// FALTA HACER EL PRODUCTO
     verTabla($tabla2);
 
     
