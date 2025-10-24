ffc9fcd09b1debdd4709bbe8c6ed5c0093a25fe5
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-10-24 14:25:35 +0200

    trabajando en BaseN

--- DWE-Servidor/forms/basen/basen.php
+++ DWE-Servidor/forms/basen/basen.php
@@ -7,30 +7,33 @@
 </head>
 <body>
 
-<?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $num = test_input($_SERVER["num"]);
-    }
+<h1>Cambio de Base</h1>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <label for="numero">Número</label>
+    <input id="numero" name="num" type="text" required>
+    <label for="base_num">Nueva Base</label>
+    <input id="base_num" name="base" type="text" required>
 
-?>
-
-<h1>Cambio Base</h1>
-<form action="">
-    <input type="text"><input type="text">
+    <input type="submit" value="Cambiar Base"> <input type="reset" value="Borrar">
 </form>
 
-
 <?php
-    echo "<h2>Your Input:</h2>";
-    echo $name;
-    echo "<br>";
-    echo $email;
-    echo "<br>";
-    echo $website;
-    echo "<br>";
-    echo $comment;
-    echo "<br>";
-    echo $gender;
+    include '../../Otros/funciones.php'; // incluye función limpiar_campos()
+    
+    function ver($num,$base1,$base2){
+        echo "<h3>Resultado:</h3>";
+        echo "Número ",$num," en base ",$base1," = ",base_convert($num,$base1,$base2)," en base ",$base2;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num = limpiar_campos($_POST["num"]);
+        $base1 = (int) substr($num,strpos($num,"/")+1);
+        $base2 = limpiar_campos($_POST["base"]);      
+        ver($num,$base1,$base2);
+    }
+    
+    
+    
 ?>
     
 </body>
