dc660e136bf8e08158631e1aa9ecb2a31a7cf901
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-10-24 10:24:47 +0200

    Ej 8 FUNCIONA

--- DWE-Servidor/Arrays/MULTIdimensionales/ej8am.php
+++ DWE-Servidor/Arrays/MULTIdimensionales/ej8am.php
@@ -5,24 +5,24 @@
 <?php
     include '../../Otros/funciones.php'; // inlucye verTabla()verTabla($tabla);
 
-    $f = 2; // filas del array
+    $f = 3; // filas del array
     $c = 3; // columnas del array
 
     for ($i=0; $i < $f; $i++) {  // Matriz 1
         for ($j=0; $j < $c; $j++) {  
-            $tabla1[$i][$j] = random_int(0,3);
+            $tabla1[$i][$j] = random_int(0,10);
         }
     } 
     for ($i=0; $i < $c; $i++) {  // Matriz 2
         for ($j=0; $j < $f; $j++) {  
-            $tabla2[$i][$j] = random_int(0,3);
+            $tabla2[$i][$j] = random_int(0,10);
         }
     }  
     echo "<h2>Matriz 1</h2>";
     verTabla($tabla1);
     echo "<br><h2>Matriz 2</h2>";
     verTabla($tabla2);
-/*
+
     $tabla3 = $tabla2; // $tabla3, es la matriz variable sobre la que mostraremos el resultado de la suma
     foreach ($tabla1 as $i => $seccion) {
         foreach ($seccion as $j => $value) {
@@ -43,24 +43,22 @@ Leyenda:
 
     tabla[filas][columnas]
 */ 
-echo "<h3>",count($tabla2),"</h3>";
+    for ($i=0; $i < $f; $i++) {  // Matriz 4 para guardar Producto (lko inicializo a 0)
+        for ($j=0; $j < $f; $j++) {  
+            $tabla4[$i][$j] = 0;
+        }
+    }
     for ($fila=0; $fila < count($tabla1); $fila++) { 
         for ($columna=0; $columna < count($tabla2[0]); $columna++) { 
-            /* ************ ZONA ERROR ********** */
             $ja = $fila; // filas
             $jb = $columna; // columnas
-            for ($i=0; $i < count($tabla1); $i++) { 
-              $tabla4[$fila][$columna]= $tabla1[$ja][$i] * $tabla2[$jb][$i];  
-              echo "<br><< ",$tabla1[$ja][$i]," * ",$tabla2[$i][$jb]," >>"; // Nota: No lee la última columna, y no la suma
+            for ($i=0; $i < count($tabla1[0]); $i++) { 
+              $tabla4[$fila][$columna]+= $tabla1[$ja][$i] * $tabla2[$i][$jb];  
             }
-            /* ************ ZONA ERROR ********** */
-            echo "<br> ·······················································";
         }
     }
 
-
-
-    echo "<br><h2>Producto</h2>";// FALTA HACER EL PRODUCTO
+    echo "<br><h2>Producto</h2>";
     verTabla($tabla4);
 
     
