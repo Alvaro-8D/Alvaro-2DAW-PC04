1f3edfd5497e283c57502e6d1c28ebdb5d376eb2
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-11-13 09:15:54 +0100

    Seguimos con SQL, segundo dia

--- BD-SQL/PHP - MySQL/mysql5pdo.php
+++ BD-SQL/PHP - MySQL/mysql5pdo.php
@@ -9,8 +9,12 @@ $dbname = "empleados1n";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT cod_dpto, nombre_dpto FROM departamento");
-    $stmt->execute();
+    $stmt = $conn->prepare("SELECT cod_dpto, nombre_dpto FROM departamento where cod_dpto=:cod");// ":cod" es una variable que podemos cambiar
+
+    $stmt->bindParam(':cod',$codigo);// variar parte de la consulta SQL
+    $codigo = "D002";// asignar valor a la variable SQL
+
+    $stmt->execute();// ejecuta la sentencia
 
     // set the resulting array to associative
      $stmt->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
