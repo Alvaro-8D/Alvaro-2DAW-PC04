3254d4a9030602d376e86a548764ad1667eb3d19
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-11-19 10:35:15 +0100

    Codigo de categoria avanzo

--- BD-SQL/DWES_UT3_WebCompras/comaltacat/fun_comaltacat.php
+++ BD-SQL/DWES_UT3_WebCompras/comaltacat/fun_comaltacat.php
@@ -3,31 +3,38 @@
         $consulta = conexionBD();
 
         try {
-            $sentencia = $consulta->prepare("INSERT INTO categoria (ID_CATEGORIA, NOMBRE) VALUES (:id,:nombre)");
-
-            $sentencia->bindParam(':id',$nuevoID);// variar parte de la consulta SQL
-            $sentencia->bindParam(':nombre',$nombre);// variar parte de la consulta SQL
-            
-            $nuevoID = 5;
+            /* * * * * * Generar Nuevo ID para Nueva Categoria * * * * * * * * */
             $sentencia = $consulta->prepare("select max(id_categoria) ultimo_id from categoria;");
-            // set the resulting array to associative
             $sentencia->execute();// ejecuta la sentencia
             $sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
             $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo
-            //$nuevoID = $resultado["ultimo_id"][0];
-            var_dump($resultado["ultimo_id"][0]);
-
-            $sentencia->execute();// ejecuta la sentencia
-
-            $sentencia = $consulta->prepare("select * from categoria;");
-            // set the resulting array to associative
-            $sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
-            $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo
-
-            echo "<h2>Cartegorias</h2>";
-            foreach($resultado as $row) {
-                echo "Codigo Categoría: " . $row["id_categoria"]. " -> Nombre: " . $row["nombre"]. "<br>";
+            if($resultado[0]["ultimo_id"] == null){
+                echo "esto es NULL";
+                $nuevoID = "C-001";
+            }else{
+                $a = intval(substr($resultado[0]["ultimo_id"],2)) + 1 ; // saca el útimo id y le suma +1 para generar el siguiente ID
+                var_dump($a);
+                $nuevoID = "C-".str_pad($a,3,0,STR_PAD_LEFT); // saca el útimo id y le suma +1 para generar el siguiente ID
+                var_dump($nuevoID);
             }
+            //
+            //
+            ///* * * * * * Insertar Categoría * * * * * * * * */
+            //$sentencia = $consulta->prepare("INSERT INTO categoria (id_categoria, nombre) VALUES (:id,:nombre)");
+            //$sentencia->bindParam(':id',$nuevoID);// variar parte de la consulta SQL
+            //$sentencia->bindParam(':nombre',$nombre);// variar parte de la consulta SQL
+            //$sentencia->execute();// ejecuta la sentencia
+//
+            ///* * * * * * Mostrar Categorías * * * * * * * * */
+            //$sentencia = $consulta->prepare("select * from categoria;");
+            //$sentencia->execute();// ejecuta la sentencia
+            //$sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
+            //$resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo
+            //
+            //echo "<h2>Cartegorias</h2>";
+            //foreach($resultado as $row) {
+            //    echo "Codigo Categoría: " . $row["ID_CATEGORIA"]. " -> Nombre: " . $row["NOMBRE"]. "<br>";
+            //}
         }
         catch(PDOException $e) {
             echo "Error: " . $e->getMessage();
