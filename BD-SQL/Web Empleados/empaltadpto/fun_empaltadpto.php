67a80fe36264482fd9ef5d3b39388f08e0f89eba
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-11-24 13:10:08 +0100

    termino empaltadpto

--- BD-SQL/Web Empleados/empaltadpto/fun_empaltadpto.php
+++ BD-SQL/Web Empleados/empaltadpto/fun_empaltadpto.php
@@ -3,22 +3,21 @@
         // Funcion principal del programa, da de alta departamentos
         $consulta = conexionBD();
         try {
-            $nuevoID = ultimo_id($consulta); // Genera un Nuevo ID (NO repetido)  
-            var_dump($nuevoID);         
-            //insertar_producto($consulta,$nombre,$nuevoID,$precio,$categoria); // Inserta el Nuevo Producto 
-            //mostrar_producto($consulta); // Mostrar las Categorias de las BD
+            $nuevoID = ultimo_id($consulta); // Genera un Nuevo ID (NO repetido)          
+            insertar_dpto($consulta,$nombre,$nuevoID); // Inserta el Nuevo Producto 
+            mostrar_dpto($consulta); // Mostrar las Categorias de las BD
         }
         catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
+            if($e->getCode() == "23000"){
+                echo "NO puede haber dos Departamentos con el MISMO CÓDIGO";
+            }else{
+               echo "Error: " . $e->getMessage(); 
+            }
+            $consulta->rollBack();
+            
         }
         $consulta = null;
     }
-
-    
-/*********************** VOY POR INSERTAR PRODUCTO********************************************* */
-// NOTA IMPORTANTE ----> TENGO QUE INLUIR COMMIT; ROLLBACK; Y CAPTAR LOS ERRORES
-
-
 
     function ultimo_id($consulta){
         // Extrae el último ID y devuelve un nuevo ID no repetido a partir del último ID
@@ -30,35 +29,36 @@
             $nuevoID = "D001";
         }else{
             // saca el útimo id y le suma +1 para generar el siguiente ID
-            $nuevoID = "D".str_pad($resultado[0]["ultimo_id"],4,0,STR_PAD_LEFT); 
+            $nuevoID = "D".str_pad(intval(substr($resultado[0]["ultimo_id"],1))+1,3,0,STR_PAD_LEFT); 
         }
         $consulta = null;
         return $nuevoID;
     }
 
-    function insertar_producto($consulta,$nombre,$nuevoID){ 
+    function insertar_dpto($consulta,$nombre,$nuevoID){ 
         // Pide el nombre y el ID, e inserta el nuevo producto en la BD 
-        $sentencia = $consulta->prepare("INSERT INTO producto (id_producto, nombre, precio, id_categoria) 
-                                            VALUES (:id,:nombre,:precio,:id_categoria)");
-        $sentencia->bindParam(':id',$nuevoID);// variar parte de la consulta SQL
+        $consulta->beginTransaction();
+        $sentencia = $consulta->prepare("INSERT INTO departamento (cod_dpto, nombre_dpto) 
+                                            VALUES (:codigo,:nombre)");
+
+        // $nuevoID='D011';// PRUEBA PARA METER UNA CLAVE DUCPLICADA
+
+        $sentencia->bindParam(':codigo',$nuevoID);// variar parte de la consulta SQL
         $sentencia->bindParam(':nombre',$nombre);// variar parte de la consulta SQL
-        $sentencia->bindParam(':precio',$precio);// variar parte de la consulta SQL
-        $sentencia->bindParam(':id_categoria',$categoria);// variar parte de la consulta SQL
         $sentencia->execute();// ejecuta la sentencia
-        return $nuevoID;
+        $consulta->commit();
         $consulta = null;
     }
 
-    function mostrar_producto($consulta){ 
+    function mostrar_dpto($consulta){ 
         // Extrae todas los Productos de la BD y las muestra por pantalla
-        $sentencia = $consulta->prepare("select * from producto order by id_producto;");
+        $sentencia = $consulta->prepare("select cod_dpto,nombre_dpto from departamento order by cod_dpto;");
         $sentencia->execute();// ejecuta la sentencia
         $sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
         $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo
         echo "<h2>Productos</h2>";
         foreach($resultado as $row) {
-            echo "Codigo Producto: ".$row["ID_PRODUCTO"]."<br>-> Nombre: ".$row["NOMBRE"]."<br>-> Precio: ";
-            echo $row["PRECIO"]."<br>-> Nombre: ".$row["ID_CATEGORIA"]."<br>>>>---------------------------------><br>";
+            echo "Codigo Departamento: ".$row["cod_dpto"]."<br>-> Nombre: ".$row["nombre_dpto"]."<br>>>>---------------------------------><br>";
         }
         $consulta = null;
     }
