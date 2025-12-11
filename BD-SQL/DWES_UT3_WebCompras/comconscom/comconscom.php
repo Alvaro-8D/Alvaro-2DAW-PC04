0980359c00af1a4cd5e0022498489ebbb1358ecb
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-12-11 10:42:54 +0100

    Avanzo en Ver Compras de un Cliente

--- BD-SQL/DWES_UT3_WebCompras/comconscom/comconscom.php
+++ BD-SQL/DWES_UT3_WebCompras/comconscom/comconscom.php
@@ -3,21 +3,25 @@
 <?php require '../fun_comunes.php'; include 'fun_comconscom.php';?>
 
 <form name='mi_formulario' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>' method='post'>
-    <p>Almacenes : 
-        <select name="almacen">
+    <p>Cliente : 
+        <select name="cliente">
             <!-- Extrae los almacenes de la BD y las mestra en el HTMl -->
-            <?php extraerAlmacenes(); ?>
+            <?php extraerClientes(); ?>
         </select>
     </p>
+    <p>Fecha DESDE:  <input name="fecha_inicio" type="date" required></p>
+    <p>Fecha HASTA:  <input name="fecha_fin" type="date" required></p>
     <input type="submit" value="Mostrar STOCK del Almacén" />
 </form>
 
 <?php
 // HACER PRIMERO "ALTA CLIENTES" Y "COMPRA DE PRODUCTOS"
     if ($_SERVER["REQUEST_METHOD"] == "POST") {    
-        $almacen = limpiar_campos($_POST['almacen']);
+        $cliente = limpiar_campos($_POST['cliente']);
+        $fecha_inicio = limpiar_campos($_POST['fecha_inicio']);
+        $fecha_fin = limpiar_campos($_POST['fecha_fin']);
 
-        consultar_almacen($almacen); //realiza todo el programa de Introducir Productos
+        compras_de_cliente($cliente,$fecha_inicio,$fecha_fin); //realiza todo el programa de Introducir Productos
     }
     
 ?>
--- BD-SQL/DWES_UT3_WebCompras/comconscom/fun_comconscom.php
+++ BD-SQL/DWES_UT3_WebCompras/comconscom/fun_comconscom.php
@@ -1,22 +1,22 @@
 <?php
-    function extraerAlmacenes(){
+    function extraerClientes(){
         $consulta = conexionBD();
         // Extrae las categorias de la BD y las muestra en el HTMl
-        $sentencia = $consulta->prepare("select * from almacen order by NUM_ALMACEN;");
+        $sentencia = $consulta->prepare("select NIF,NOMBRE from cliente order by NIF;");
         $sentencia->execute();// ejecuta la sentencia
         $sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
-        $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo    
+        $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo   
         foreach ($resultado as $key => $value) {
-            echo "<option value=\"",$value["NUM_ALMACEN"],"\">",$value["LOCALIDAD"],"</option>";
+            echo "<option value=\"",$value["NIF"],"\">",$value["NOMBRE"],"(",$value["NIF"],")","</option>";
         }
         $consulta = null;
     }
   
-    function  consultar_almacen($almacen){
-        // Funcion principal del programa, ver STOCK de un Almacén
+    function  compras_de_cliente($cliente,$fecha_inicio,$fecha_fin){
+        // Funcion principal del programa, ver compras de un cliente
         $consulta = conexionBD();
         try {
-            info_almacen($consulta,$almacen); // Mostrar las Categorias de las BD
+            ver_compras($consulta,$cliente,$fecha_inicio,$fecha_fin); // muestra las compras de ese clientes entre dos fechas
         }
         catch(PDOException $e) {
             echo "Error: " . $e->getMessage();
@@ -24,25 +24,24 @@
         $consulta = null;
     }
 
-    function info_almacen($consulta,$almacen){ 
+    function ver_compras($consulta,$cliente,$fecha_inicio,$fecha_fin){ 
         // Extrae el STOCK de la BD y las muestra por pantalla
-    
-        //select NOMBRE,CANTIDAD,LOCALIDAD
-        //from producto p, almacena a2, almacen a1
-        //where a2.NUM_ALMACEN = 1 && a2.ID_PRODUCTO = p.ID_PRODUCTO && a2.NUM_ALMACEN = a1.NUM_ALMACEN;
-        
-        $sentencia = $consulta->prepare("select NOMBRE,CANTIDAD,LOCALIDAD
-                                            from producto p, almacena a2, almacen a1
-                                            where a2.NUM_ALMACEN = :num_almacen && a2.ID_PRODUCTO = p.ID_PRODUCTO && a2.NUM_ALMACEN = a1.NUM_ALMACEN;");
-        $sentencia->bindParam(':num_almacen',$almacen);
+        $sentencia = $consulta->prepare("SELECT CONCAT(' + ',NOMBRE,' (',c.ID_PRODUCTO,') >> ',UNIDADES,' x ',PRECIO,' Euros/unidad = ',PRECIO*UNIDADES)
+                                        FROM producto p, compra c
+                                        WHERE p.ID_PRODUCTO = c.ID_PRODUCTO AND c.NIF = :cliente 
+                                        AND FECHA_COMPRA BETWEEN :fecha_inicio AND :fecha_fin ;");
+        $sentencia->bindParam(':cliente',$cliente);
+        $sentencia->bindParam(':fecha_inicio',$fecha_inicio);
+        $sentencia->bindParam(':fecha_fin',$fecha_fin);
         $sentencia->execute();// ejecuta la sentencia
         $sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
         $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo
 
         if($resultado===array()){
-            echo "<h2>No hay STOCK en el almacén >>".recuperar_almacen($consulta,$almacen)."<<</h2>";
+            echo "<h2>No hay compras de >>".recuperar_cliente($consulta,$cliente)."<< 
+            ente ("") y ()</h2>";
         }else{
-            echo "<h2>Stock del Almacen >>".$resultado[0]["LOCALIDAD"]."<<</h2>";
+            echo "<h2>Stock del Almacen >>".recuperar_cliente($consulta,$cliente)."<<</h2>";
             foreach ($resultado as $key => $value) {
                 echo "<h3>".$value["NOMBRE"]." ===> ".$value["CANTIDAD"]."</h3>";
             }  
@@ -51,16 +50,43 @@
         $consulta = null;
     }
 
-    function recuperar_almacen($consulta,$almacen){ 
+    function recuperar_cliente($consulta,$dni){ 
         // Recuperar Nombre del Almacen
-        $sentencia = $consulta->prepare("select LOCALIDAD from almacen where NUM_ALMACEN = :num_almacen;");
-        $sentencia->bindParam(':num_almacen',$almacen);
+        $sentencia = $consulta->prepare("select NOMBRE from cliente where NIF = :dni;");
+        $sentencia->bindParam(':dni',$dni);
         $sentencia->execute();// ejecuta la sentencia
         $sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
         $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo
 
         $consulta = null;
-        return $resultado[0]["LOCALIDAD"];
+        return $resultado[0]["NOMBRE"];
     }
+
+    function suma_de_compras($consulta,$cliente,$fecha_inicio,$fecha_fin){ 
+        // Extrae el STOCK de la BD y las muestra por pantalla
+        $sentencia = $consulta->prepare("SELECT CONCAT(' + ',NOMBRE,' (',c.ID_PRODUCTO,') >> ',UNIDADES,' x ',PRECIO,' Euros/unidad = ',PRECIO*UNIDADES)
+                                        FROM producto p, compra c
+                                        WHERE p.ID_PRODUCTO = c.ID_PRODUCTO AND c.NIF = :cliente 
+                                        AND FECHA_COMPRA BETWEEN :fecha_inicio AND :fecha_fin ;");
+        $sentencia->bindParam(':cliente',$cliente);
+        $sentencia->bindParam(':fecha_inicio',$fecha_inicio);
+        $sentencia->bindParam(':fecha_fin',$fecha_fin);
+        $sentencia->execute();// ejecuta la sentencia
+        $sentencia->setFetchMode(PDO::FETCH_ASSOC); // modo de recuperar los datos de la select
+        $resultado=$sentencia->fetchAll(); // guardar la sida de la select en un Array Asociativo
+
+        if($resultado===array()){
+            echo "<h2>No hay compras de >>".recuperar_cliente($consulta,$cliente)."<< 
+            ente ("") y ()</h2>";
+        }else{
+            echo "<h2>Stock del Almacen >>".recuperar_cliente($consulta,$cliente)."<<</h2>";
+            foreach ($resultado as $key => $value) {
+                echo "<h3>".$value["NOMBRE"]." ===> ".$value["CANTIDAD"]."</h3>";
+            }  
+        }
+        
+        $consulta = null;
+    }
+
 
 ?>
